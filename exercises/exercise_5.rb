e5f1dfa5f448274443196092c4d2e4a61713e9bf
carolineolagunju <olamidejr21@gmail.com>
2023-10-22 13:08:19 -0600

    write query to calculate total revenue, average revenue and count how many stores that make M or more in revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# calculate total revenueof all stores
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: #{@total_revenue}"
+
+# calculate average revenueof all stores
+@average_revenue = Store.average(:annual_revenue)
+puts "Average Revenue: #{@average_revenue}"
+
+# returns number of stores that make $1M or more in revenue
+@over_1M_revenue = Store.where('annual_revenue >= ?', 1000000).count
+puts "The number of stores that are generating $1M or more in annual sales is #{@over_1M_revenue}"
