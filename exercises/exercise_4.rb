debfffab37aeef100bc67eaaddd0b234f4b79f71
carolineolagunju <olamidejr21@gmail.com>
2023-10-22 12:43:48 -0600

    create store Surrey, Whistler, Yaletown, fetch men's store and women's store as @mens_stores and @womens_stores and print puts the annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# create store Surrey, Whistler and Yaletown
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that only carries men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# return the annual revenue of each store that only carries men's apparel
+@mens_stores.each do |store|
+  puts "#{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+# Fetch stores that only carries women's apparel and are generating less than $1M in annual revenue
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue < ?', 1000000)
+
+# return the annual revenue of each store that only carries women's apparel and annual revenue is less than $1M
+@womens_stores.each do |store|
+  puts "#{store.name} Annual Revenue: #{store.annual_revenue}"
+end
