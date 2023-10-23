4059a084fe52da0bcd869519d5b51dde368934b6
carolineolagunju <olamidejr21@gmail.com>
2023-10-22 19:19:57 -0600

    add test code

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(last_name: "Santiago", hourly_rate: 50)
+@store1.employees.create(first_name: "Pamela", hourly_rate: 50)
+@store1.employees.create(last_name: "Santiago", first_name: "Pamela", hourly_rate: 202)
+Employee.create(last_name: "Santiago", first_name: "Pamela", hourly_rate: 200)
+Store.create(name: "Ba", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Super Store", annual_revenue: -1, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Super Store", annual_revenue: 30, mens_apparel: false, womens_apparel: false)
+
+puts "Store Name?"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
